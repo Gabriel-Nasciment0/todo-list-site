6a31ceb0457143bc38b8932d4af3d1687a9d09a4
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2026-04-13 23:39:00 -0300

    .

---
+++ src/components/Column.jsx
@@ -0,0 +1,4 @@
+export default function Column(tasks, status) {
+    const filteredTasks = tasks.filter((tasks) => tasks.status === status)
+    return()
+}
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+export default function Dashboard(tasks, setTasks) {
+    console.log(tasks)
+    return (
+        <div>
+            <div style={{ display: "flex" }}>To Do</div>
+            <div style={{ display: "flex" }}>Progress</div>
+            <div style={{ display: "flex" }}>Done</div>
+        </div>
+
+    )
+}
