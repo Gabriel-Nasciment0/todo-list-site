a339ae71f1b6f9d4c88186121c1866faf82283a9
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2026-04-16 15:12:12 -0300

    .

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -25,63 +25,73 @@ export default function Dashboard({ tasks, setTasks }) {
             (filter === "done" && task.status === "done") ||
             (filter === "pending" && task.status !== "done"),
     )
+
     return (
-        <div>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Nova Task"
-                    value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}
-                />
-                <input
-                    type="date"
-                    value={dueDate}
-                    onChange={(e) => setDueDate(e.target.value)}
-                />
-                <button onClick={handleAddTask}>Adicionar</button>
-            </div>
-            <div>
-                <button
-                    onClick={() => setFilter("all")}
-                    style={{ fontWeight: filter === "all" ? "bold" : "normal" }}
-                >
-                    Todas
-                </button>
-                <button
-                    onClick={() => setFilter("done")}
-                    style={{
-                        fontWeight: filter === "done" ? "bold" : "normal",
-                    }}
-                >
-                    Concluídas
-                </button>
-                <button
-                    onClick={() => setFilter("pending")}
-                    style={{
-                        fontWeight: filter === "pending" ? "bold" : "normal",
-                    }}
-                >
-                    Pendentes
-                </button>
-            </div>
-            <div style={{ display: "flex", gap: "20px" }}>
-                <Column
-                    status="todo"
-                    setTasks={setTasks}
-                    tasks={filteredTasks}
-                />
-                <Column
-                    status="progress"
-                    setTasks={setTasks}
-                    tasks={filteredTasks}
-                />
-                <Column
-                    status="done"
-                    setTasks={setTasks}
-                    tasks={filteredTasks}
-                />
-            </div>
+        <div className="container">
+            <aside className="sidebar">
+                <h2>projects</h2>
+                <p>To Do</p>
+                <p>In Progress</p>
+                <p>Done</p>
+            </aside>
+            <main className="main">
+                <header className="header">
+                    <input
+                        type="text"
+                        placeholder="Nova Task"
+                        value={newTask}
+                        onChange={(e) => setNewTask(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        value={dueDate}
+                        onChange={(e) => setDueDate(e.target.value)}
+                    />
+                    <button onClick={handleAddTask}>Adicionar</button>
+                    <button
+                        onClick={() => setFilter("all")}
+                        style={{
+                            fontWeight: filter === "all" ? "bold" : "normal",
+                        }}
+                    >
+                        Todas
+                    </button>
+                    <button
+                        onClick={() => setFilter("done")}
+                        style={{
+                            fontWeight: filter === "done" ? "bold" : "normal",
+                        }}
+                    >
+                        Concluídas
+                    </button>
+                    <button
+                        onClick={() => setFilter("pending")}
+                        style={{
+                            fontWeight:
+                                filter === "pending" ? "bold" : "normal",
+                        }}
+                    >
+                        Pendentes
+                    </button>
+                </header>
+                <section className="board">
+                    <Column
+                        status="todo"
+                        setTasks={setTasks}
+                        tasks={filteredTasks}
+                    />
+                    <Column
+                        status="progress"
+                        setTasks={setTasks}
+                        tasks={filteredTasks}
+                    />
+                    <Column
+                        status="done"
+                        setTasks={setTasks}
+                        tasks={filteredTasks}
+                    />
+                </section>
+            </main>
         </div>
     )
 }
